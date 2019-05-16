8a8f487ebce1e4038eb691cf74ddc1f3cf63d898
kjuvi <quentin.vaucher@protonmail.com>
2019-05-16 23:17:35 +0200

    refactor preprocessing into a function

--- WikipediaTopicLabeling/src/main/scala/WikipediaTopicLabeling.scala
+++ WikipediaTopicLabeling/src/main/scala/WikipediaTopicLabeling.scala
@@ -1,43 +1,48 @@
 import org.apache.spark.ml.feature.{HashingTF, IDF, RegexTokenizer, StopWordsRemover}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 
 object WikipediaTopicLabeling {
+
+    def preprocessing(spark: SparkSession, path: String): DataFrame = {
+        // load json
+        val jsonData = spark.sqlContext.read.json(path)
+
+        // tokenizer
+        val regexTokenizer = new RegexTokenizer()
+          .setInputCol("text")
+          .setOutputCol("raw words")
+          .setPattern("\\W")
+
+        val tokenizedData = regexTokenizer.transform(jsonData)
+
+        // stop words removal
+        val remover = new StopWordsRemover()
+          .setInputCol("raw words")
+          .setOutputCol("words")
+
+        val preprocessedData = remover.transform(tokenizedData)
+
+        // TF-IDF
+        val hashingTF = new HashingTF()
+          .setInputCol("words")
+          .setOutputCol("raw features")
+          .setNumFeatures(20)
+
+        val featurizedData = hashingTF.transform(preprocessedData)
+
+        val idf = new IDF().setInputCol("raw features").setOutputCol("features")
+        val idfModel = idf.fit(featurizedData)
+
+        idfModel.transform(featurizedData)
+    }
 
   def main(args: Array[String]) {
     val spark = SparkSession.builder.appName("Wikipedia Topic Labeling").getOrCreate()
 
-    // load json
-    val articles = spark.sqlContext.read.json("data/wiki.json")
+    val data = preprocessing(spark, "data/wiki.json")
 
-    // tokenizer
-    val regexTokenizer = new RegexTokenizer()
-      .setInputCol("text")
-      .setOutputCol("raw words")
-      .setPattern("\\W")
-
-    val articlesTokenized = regexTokenizer.transform(articles)
-
-    // stop words removal
-    val remover = new StopWordsRemover()
-      .setInputCol("raw words")
-      .setOutputCol("words")
-
-    val articlesCleaned = remover.transform(articlesTokenized)
-
-    // TF-IDF
-    val hashingTF = new HashingTF()
-      .setInputCol("words")
-      .setOutputCol("raw features")
-      .setNumFeatures(20)
-
-    val featurizedData = hashingTF.transform(articlesCleaned)
-
-    val idf = new IDF().setInputCol("raw features").setOutputCol("features")
-    val idfModel = idf.fit(featurizedData)
-
-    val rescaledData = idfModel.transform(featurizedData)
-
-    println(rescaledData.show)
+    println(data.select("raw features").show)
 
     spark.stop()
   }
