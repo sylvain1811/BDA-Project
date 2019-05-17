9ad6e068e806851b1a16819811c4e0ae706f9c8d
Sylvain Renaud <sylvain.renaud@master.hes-so.ch>
2019-05-17 17:30:29 +0200

    Update run_wiki_extraxtor.sh script.

--- run_wiki_extraxtor.sh
+++ run_wiki_extraxtor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 wikiextractor/WikiExtractor.py -o data/wiki --process 8 data/enwiki-latest-pages-articles1.xml-p10p30302
+python3 wikiextractor/WikiExtractor.py -o data/wiki --process 12 --json data/enwiki-latest-pages-articles1.xml-p10p30302.bz2
